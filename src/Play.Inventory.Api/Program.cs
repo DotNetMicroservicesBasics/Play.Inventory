454adc93696d67f44eccec8471497090c379112a
yasserMokh <yasser.moukhtar@outlook.com>
2022-11-30 10:54:10 +0200

    resource exahustion resilience

--- src/Play.Inventory.Api/Program.cs
+++ src/Play.Inventory.Api/Program.cs
@@ -1,6 +1,8 @@
 using Play.Common.Data;
 using Play.Inventory.Api.Clients;
 using Play.Inventory.Data.Entities;
+using Polly;
+using Polly.Timeout;
 
 namespace Play.Catalog.Api;
 
@@ -16,10 +18,33 @@ public class Program
         builder.Services.AddMongoDb()
                         .AddMongoRepository<InventoryItem>("InventoryItems");
 
+        Random jitterer = new Random();
+
         builder.Services.AddHttpClient<CatalogClient>(client =>
         {
             client.BaseAddress = new Uri("https://localhost:7040");
-        });
+        })
+        .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+            5,
+            retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+            onRetry: (outcome, timespan, retryAttempt) =>
+            {
+                Console.WriteLine($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+            }
+        ))
+        .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+            3,
+            TimeSpan.FromSeconds(15),
+            onBreak: (outcome, timespan) =>
+            {
+                Console.WriteLine($"Opening the circuit for {timespan.TotalSeconds} seconds...");
+            },
+            onReset: () =>
+            {
+                Console.WriteLine($"Closing the circuit...");
+            }
+        ))
+        .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
         builder.Services.AddControllers(options =>
         {
